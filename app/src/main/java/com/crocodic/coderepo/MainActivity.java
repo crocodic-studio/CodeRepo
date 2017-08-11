d2aaf4aa2ba22746f69148599f943072ddafa51b
Zidd <yzzzd2011@gmail.com>
2017-08-11 16:31:43 +0700

    First commit

--- app/src/main/java/com/crocodic/coderepo/MainActivity.java
+++ app/src/main/java/com/crocodic/coderepo/MainActivity.java
@@ -2,12 +2,17 @@ package com.crocodic.coderepo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHalo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtHalo = (TextView) findViewById(R.id.txt_halo);
     }
 }
