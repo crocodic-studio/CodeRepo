cb6fc6dae198fef5354f70a5df713ecdfeabc19e
ridwan23 <ridwansantos23@gmail.com>
2017-08-12 11:35:45 +0700

    initial picasso sample

---
+++ app/src/main/java/com/crocodic/coderepo/picasso/PicassoSampleActivity.java
@@ -0,0 +1,15 @@
+package com.crocodic.coderepo.picasso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.crocodic.coderepo.R;
+
+public class PicassoSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picasso_sample);
+    }
+}
